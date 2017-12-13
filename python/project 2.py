eebb932e210bc61cf86ec2a5b003b55570402c25
sumangala028 <sumangala028@gmail.com>
2017-12-13 20:18:05 +0530

    Initial Commit

---
+++ python/project 2.py
@@ -0,0 +1,23 @@
+f = open("data.txt", "r")
+
+for line in f:
+    print (line)
+
+newlist = f.split()
+for li in newlist:
+    print (li)
+qty = input("Please enter item quantity :")
+print ("Bill statement")
+print ("Bill ID", i)
+from datetime import datetime
+now = datetime.now()
+print ("Date : %s-%s-%s  %s:%s\n" %((now.year),(now.month), (now.day),(now.hour), (now.minute)))
+print ("Item\tQty\tTot\n")
+     i = i + 1
+        order = input("Please enter the item ID :")
+        
+
+
+
+    
+    
