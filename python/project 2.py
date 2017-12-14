8736a0e8030b3f352038934e45afbbb9d75739e8
sumangala028 <sumangala028@gmail.com>
2017-12-14 16:32:51 +0530

    Error in output line

--- python/project 2.py
+++ python/project 2.py
@@ -1,26 +1,37 @@
-bill = open("data.txt", "r").read().split(",")
-
+bill = open("data.txt" , "r")
 print ("\tROLEX BAKE MART")
-print ("ID\tItem\tPrice\tStock\n")
-for li in bill:
-    print (li,end='\t')
-    
-order = input("\nPLese enter the item ID: ")
-print (li[0])
+print ("ID\tItem\tPrice\tStock")
 
-qty = input("Please enter item quantity :")
-print ("Bill statement")
+    
+itemIdList = []
+itemNameList = []
+itemPriceList = []
+for line in bill:
+    lineList = line.split(",")
+    itemIdList.append(lineList[0])
+    itemNameList.append(lineList[1])
+    itemPriceList.append(lineList[2])
+print (itemIdList)
+print (itemNameList)
+print (itemPriceList)
+
 i = 1
-print ("Bill ID", i)
-from datetime import datetime
-now = datetime.now()
-print ("Date : %s-%s-%s  %s:%s\n" %((now.year),(now.month), (now.day),(now.hour), (now.minute)))
-print ("Item\tQty\tTot\n")
-print ("%s\t%s\t%s" % qty)
-i = i + 1
-        
-
-
-
-    
+itemId = -1
+while (itemId != 0):
+    itemId = input("Please Input Item ID :")
+    if (itemId in itemIdList):
+        index = itemIdList.index(itemId)
+        print (itemId,end='\t')
+        print (itemNameList[index],end='\t')
+        print ("RS.",itemPriceList[index])
+        qty = input("Please Input the quantity")
+    else :
+        print ("Bill Statement")
+        print ("Bill ID :", i)
+        from datetime import datetime
+        now = datetime.now()
+        print ("Date : %s-%s-%s  %s:%s\n" %((now.year),(now.month), (now.day),(now.hour), (now.minute)))
+        print ("\nItem\tQTY\tTot")
+        print ("%s\t%s\t%s" % itemId, qty,itemPriceList[index]*qty)
+        i = i + 1
     
--- python/tmp1.py
+++ python/tmp1.py
@@ -1,3 +1,19 @@
 bill = open("data.txt", "r")
-for li in bill:
-    print (li)
+itemIdList = []
+itemNameList = []
+for line in bill:
+    lineList = line.split(",")
+    #print(lineList)
+    itemIdList.append(lineList[0])
+    itemNameList.append(lineList[1])
+print(itemIdList)
+print (itemNameList)
+
+itemId = -1
+while(itemId != 0 ) :
+    itemId = input("Enter ID : " )
+    index = itemIdList.index(itemId)
+    print (index)
+    print (itemNameList[index])
+
+print("END")
