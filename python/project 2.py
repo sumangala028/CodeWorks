061ce24c185a44c933628acf39a2b2eb669399a9
sumangala028 <sumangala028@gmail.com>
2017-12-15 22:53:26 +0530

    Bill print error

--- python/project 2.py
+++ python/project 2.py
@@ -6,6 +6,9 @@ print ("ID\tItem\tPrice\tStock")
 itemIdList = []
 itemNameList = []
 itemPriceList = []
+indexList = []
+qtyList = []
+total = 0
 for line in bill:
     lineList = line.split(",")
     itemIdList.append(lineList[0])
@@ -15,23 +18,36 @@ print (itemIdList)
 print (itemNameList)
 print (itemPriceList)
 
-i = 1
+i = 0
 itemId = -1
-while (itemId != 0):
+while (True):
     itemId = input("Please Input Item ID :")
     if (itemId in itemIdList):
         index = itemIdList.index(itemId)
+        indexList.append(index)
         print (itemId,end='\t')
         print (itemNameList[index],end='\t')
         print ("RS.",itemPriceList[index])
         qty = input("Please Input the quantity")
+        qtyList.append(qty)
+
     else :
-        print ("Bill Statement")
-        print ("Bill ID :", i)
-        from datetime import datetime
-        now = datetime.now()
-        print ("Date : %s-%s-%s  %s:%s\n" %((now.year),(now.month), (now.day),(now.hour), (now.minute)))
-        print ("\nItem\tQTY\tTot")
-        print ("%s\t%s\t%s" % itemId, qty,itemPriceList[index]*qty)
-        i = i + 1
+        break
+    
+def computeBill(order):
+    total = 0
+    for item in order:
+        total = itemPriceList[item] * qtyList[item]
+        return total
+    
+    print ("Bill Statement")
+    print ("Bill ID :%s" % (i+1))
+    from datetime import datetime
+    now = datetime.now()
+    print ("Date : %s-%s-%s  %s:%s\n" %((now.year),(now.month), (now.day),(now.hour), (now.minute)))
+    print ("\nItem\tQTY\tTot")
+    print ("%s\t%s\t%s" % itemIdList[item], qtyList[item], total)
+    i = i + 1
+    computeBill(index)
+
     
--- python/tmp1.py
+++ python/tmp1.py
@@ -17,3 +17,30 @@ while(itemId != 0 ) :
     print (itemNameList[index])
 
 print("END")
+
+bill = open("data.txt", "r").read().split(",")
+
+print ("\tROLEX BAKE MART")
+print ("ID\tItem\tPrice\tStock\n")
+for li in bill:
+    print (li,end='\t')
+    lineList = li.split(",")
+order = input("\nPLese enter the item ID: ")
+print (bill[order-1])
+
+while order != 0:
+    qty = input("Please enter item quantity :")
+print ("Bill statement")
+i = 1
+print ("Bill ID", i)
+from datetime import datetime
+now = datetime.now()
+print ("Date : %s-%s-%s  %s:%s\n" %((now.year),(now.month), (now.day),(now.hour), (now.minute)))
+
+print ("Item\tQty\tTot\n")
+print ("%s\t%s\t%s" % qty)
+i = i + 1
+        
+
+
+
